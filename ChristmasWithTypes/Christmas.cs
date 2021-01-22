aabcc20ffaabf07d99f51004c94e0df9d8ec2adf
Ms-Linda <wfhlinda@gmail.com>
2021-01-22 02:48:28 -0500

    changed TODO to DONE

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -7,7 +7,7 @@ namespace ChristmasWithTypes
         public string Santa { get; set; }
         public int? TreeHeight { get; set; } //DONE Make the Height property nullable
 
-        //TODO Make the property, "Day", type enum
+        //DONE Make the property, "Day", type enum
         public enum Day { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday};
 
     }
